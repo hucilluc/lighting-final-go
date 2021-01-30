6ee740fcf276db5e708a2266c38043a86f68972b
hucilluc <katehenderson@gmx.co.uk>
2021-01-30 16:16:11 +0000

    A fox in a box

---
+++ Lighting-final-go/Assets/Plugins/upload.bat
@@ -0,0 +1,1 @@
+curl -v -b cookie -X PUT -F "space_template[zip]=@/Users/Kate/Desktop/Github/lighting-final-go/Lighting-final-go/template.zip" -F "space_template[game_engine_version]=20194" https://account.altvr.com/api/space_templates/1661711079417315536.json
